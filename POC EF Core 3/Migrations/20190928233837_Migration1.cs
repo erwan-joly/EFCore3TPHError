84377f7b9a56eac832741baf27afe69ffce53532
erwan-joly <erwan.joly@pushpay.com>
2019-09-29 13:46:39 +1300

    add poc for error

---
+++ POC EF Core 3/Migrations/20190928233837_Migration1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace POC_EF_Core_3.Migrations
+{
+    public partial class Migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ItemInstance",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Amount = table.Column<short>(nullable: false),
+                    BoundCharacterId = table.Column<long>(nullable: true),
+                    CharacterId = table.Column<long>(nullable: false),
+                    Design = table.Column<short>(nullable: false),
+                    DurabilityPoint = table.Column<int>(nullable: false),
+                    ItemDeleteTime = table.Column<DateTime>(nullable: true),
+                    ItemVNum = table.Column<short>(nullable: false),
+                    Upgrade = table.Column<byte>(nullable: false),
+                    Rare = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemInstance", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemInstance");
+        }
+    }
+}
--- POC EF Core 3/Program.cs
+++ POC EF Core 3/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 
 namespace POC_EF_Core_3
@@ -12,11 +15,61 @@ namespace POC_EF_Core_3
         public Guid Id { get; set; }
     }
 
+    public static class DbContextFindAllExtensions
+    {
+        private static readonly MethodInfo ContainsMethod = typeof(Enumerable).GetMethods()
+            .FirstOrDefault(m => (m.Name == "Contains") && (m.GetParameters().Length == 2))
+            .MakeGenericMethod(typeof(object));
+
+        public static IQueryable<T> FindAllAsync<T>(this DbSet<T> dbSet, PropertyInfo keyProperty,
+            params object[] keyValues)
+            where T : class
+        {
+            // build lambda expression
+            var parameter = Expression.Parameter(typeof(T), "e");
+            var body = Expression.Call(null, ContainsMethod,
+                Expression.Constant(keyValues),
+                Expression.Convert(Expression.MakeMemberAccess(parameter, keyProperty), typeof(object)));
+            var predicateExpression = Expression.Lambda<Func<T, bool>>(body, parameter);
+
+            // run query
+            Console.WriteLine(predicateExpression.ToString());
+            return dbSet.Where(predicateExpression);
+        }
+    }
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("POC EF CORE 3");
+            try
+            {
+                using (var context = new PocDbContext())
+                {
+                    var dbkey = typeof(ItemInstance).GetProperty("Id");
+
+                    var test = context.ItemInstance.FindAllAsync(dbkey, new[] { new Guid() }).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                using (var context = new PocDbContext())
+                {
+                    Expression<Func<ItemInstance, bool>> predicateExpression = s => new[] {new Guid()}.Contains(s.Id);
+                    Console.WriteLine(predicateExpression.ToString());
+                    var test = context.ItemInstance.Where(predicateExpression).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            } 
+            Console.ReadKey();
         }
     }
 }
